f91821f836274aba48d101584ccc6743867bc6bf
Carson Ip <carsonip715@gmail.com>
2016-08-27 17:52:03 -0700

    Initial commit

---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+import { version } from '../../package.json';
+import { Router } from 'express';
+import facets from './facets';
+
+export default ({ config, db }) => {
+	let api = Router();
+
+	// mount the facets resource
+	api.use('/facets', facets({ config, db }));
+
+	// perhaps expose some API metadata at the root
+	api.get('/', (req, res) => {
+		res.json({
+			mp3: 'https://s3.amazonaws.com/alexa-music/score.mp3'
+		});
+	});
+
+	return api;
+}
