45ae60d99d55c96a47348bfa9a01199e7e72edc9
Carson Ip <carsonip715@gmail.com>
2016-08-28 09:59:00 -0700

    Now slows down

--- src/process/chop.js
+++ src/process/chop.js
@@ -21,33 +21,41 @@ export default function chop(filename, tempo, beats, measureCnt) {
       .run();
     for (var step = 1; step <= 2; step++){
       for (var i = 0, len = measureCnt; i + step <= len; i++) {
+        for (var newTempo=0.5;newTempo<=1;newTempo+=0.5){
 
+          var id = `${timestamp}-${i}-${i + step}${newTempo==0.5?'-s':''}`;
+          var out = `/tmp/${id}.mp3`;
 
-        var id = `${timestamp}-${i}-${i + step}`;
-        var out = `/tmp/${id}.mp3`;
+          var start = i * (beats / tempo) * 60, duration = (beats / tempo) * 60;
+          ffmpeg(`assets/${filename}`)
+            .inputOptions([
+              `-ss ${start}`,
+              `-t ${duration}`,
+            ])
+            .audioFilters(
+              {
+                filter: 'atempo',
+                options: newTempo
+              }
+            )
+            .output(out)
+            .on('end', ((id, out) => {return () => {
+              var params = {Bucket: 'alexa-music', Key: id, Body: fs.createReadStream(out), ACL: 'public-read'};
+              s3.putObject(params, function (err, data) {
+                if (err) console.log(err);
+                else console.log(`Successfully uploaded data to s3 ${id}`);
+              });
+            }})(id, out))
+            .run();
+        }
 
-        var start = i * (beats / tempo) * 60, duration = (beats / tempo) * 60;
-        ffmpeg(`assets/${filename}`)
-          .inputOptions([
-            `-ss ${start}`,
-            `-t ${duration}`
-          ])
-          .output(out)
-          .on('end', ((id, out) => {return () => {
-            var params = {Bucket: 'alexa-music', Key: id, Body: fs.createReadStream(out), ACL: 'public-read'};
-            s3.putObject(params, function (err, data) {
-              if (err) console.log(err);
-              else console.log(`Successfully uploaded data to s3 ${id}`);
-            });
-          }})(id, out))
-          .run();
       }
     }
   });
 
   return {
     baseUrl: "https://s3.amazonaws.com/alexa-music/",
-    baseId: timestamp,
+    baseId: '' + timestamp,
     measureCnt: measureCnt
   };
 }
